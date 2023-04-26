f8af99642788a01a4b727546bad331a555ec88fd
foxsis <va.four2016@gmail.com>
2023-04-26 09:34:08 +0300

    add pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    apps: [
-      {
-        name: 'FRONT',
-        port: '3000',
-        exec_mode: 'cluster',
-        instances: 'max',
-        script: './.output/server/index.mjs'
-      }
-    ]
-  }
+  apps: [
+    {
+      name: 'FRONT',
+      exec_mode: 'cluster',
+      instances: 'max', // Or a number of instances
+      script: './node_modules/nuxt/bin/nuxt.js',
+      args: 'start'
+    }
+  ]
+}
