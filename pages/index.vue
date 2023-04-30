b3dfcb74f70ce88a32984f1c8b2d848e697c044e
foxsis <va.four2016@gmail.com>
2023-04-30 12:38:04 +0300

    fix4.2

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div v-if="loading">Loading...</div>
   <div v-else-if="!loading" class="flex flex-col gap-20">
     <MainASlider />
-    <MainACategory :category="data.categories.data" />
+    <!-- <MainACategory :category="data.categories.data" /> -->
     <MainABrends :marka="data.markaAutos.data" />
     <MainACatalog :catalog="data.brandsAutos.data" />
   </div>
