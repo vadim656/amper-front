cdfde86d8a6b79a3b2c18e023a07955fcd8c0944
foxsis <va.four2016@gmail.com>
2023-04-30 12:39:59 +0300

    fix4.4

--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,13 @@
   <div v-else-if="!loading" class="flex flex-col gap-20">
     <MainASlider />
     <!-- <MainACategory :category="data.categories.data" /> -->
-    <MainABrends :marka="data.markaAutos.data" />
-    <MainACatalog :catalog="data.brandsAutos.data" />
+    <!-- <MainABrends :marka="data.markaAutos.data" />
+    <MainACatalog :catalog="data.brandsAutos.data" /> -->
+    <pre>{{ resMain }}</pre>
   </div>
 </template>
 <script setup>
 
 import {MARKA_ALL} from '~/gql/query/main/MARKA_ALL.js'
-const { data, loading  } = await useAsyncQuery(MARKA_ALL)
+const { data: resMain, loading  } = await useAsyncQuery(MARKA_ALL)
 </script>
