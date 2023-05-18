e955a4edc971c9bfdea291fa39aff59bd18f80e2
foxsis <va.four2016@gmail.com>
2023-05-18 19:04:41 +0300

    fix ssr

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,9 @@
   <div v-if="loading">Loading...</div>
   <div v-else-if="!loading" class="flex flex-col gap-20">
     <MainASlider />
-    <!-- <MainACategory :category="resMain.categories.data" /> -->
-    <!-- <MainABrends :marka="resMain.markaAutos.data" /> -->
-    <!-- <MainACatalog :catalog="resMain.brandsAutos.data" /> -->
+    <MainACategory :category="resMain.categories.data" />
+    <MainABrends :marka="resMain.markaAutos.data" />
+    <MainACatalog :catalog="resMain.brandsAutos.data" />
     
   </div>
 </template>
